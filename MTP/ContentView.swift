b3d94ab5f82626344eef57dd221247cae77b18b9
Ryan Gillespie <ryan.gillespie@tuta.io>
2021-05-07 03:28:22 -0400

    Initial Commit

---
+++ MTP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MTP
+//
+//  Created by Ryan Gillespie on 5/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MTP/MTPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MTPApp.swift
+//  MTP
+//
+//  Created by Ryan Gillespie on 5/7/21.
+//
+
+import SwiftUI
+
+@main
+struct MTPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
